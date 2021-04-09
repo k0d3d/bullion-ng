57b9dff4d1f52d91d55c39d1426b9a4933dd0d29
Pxlart Design <michael.rhema@gmail.com>
2021-04-09 12:44:38 +0100

    moved to vue

--- src/main.js
+++ src/main.js
@@ -3,110 +3,37 @@ import tokenContract from '../build/contracts/BullionCollectible.json';
 import Vue from 'vue'
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  //do work
-  console.log("running every day ");
-  const buttonMint = document.querySelector("#bullion-admin-mint .mint-btn");
-
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-  const signer = provider.getSigner();
-
-  buttonMint.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (provider) {
-      checkNetwork(provider)
-        .then((e) => {
-          startApp();
-        })
-        .catch((e) => console.log(e));
-    }
-  });
-});
-
 const startApp = async () => {
-  const chainId = await ethereum.request({ method: "eth_chainId" });
-  handleChainChanged(chainId);
-
-  ethereum.on("chainChanged", handleChainChanged);
-};
-
-const web3networks = {
-  1: {
-    name: "main",
-    etherscanUrl: "https://etherscan.io/tx/",
-  },
-  3: {
-    name: "ropsten",
-    etherscanUrl: "https://ropsten.etherscan.io/tx/",
-  },
-  4: {
-    name: "rinkeby",
-    etherscanUrl: "https://rinkeby.etherscan.io/tx/",
-  },
-  42: {
-    name: "kovan",
-    etherscanUrl: "https://kovan.etherscan.io/tx/",
-  },
-};
-
-let state = {
-  web3ctx: {
-    networkId: "",
-    networkName: "",
-    etherscanUrl: "",
-    activeWallet: "",
-    lastBlockNumber: "",
-    currentBalance: "",
-    tokenAddress: "0x494b2CeE761FdfCd6f5bE1ABefB7A6112B251874",
-    ipfsGateway: "https://ipfs.infura.io/ipfs/",
-  },
+  return await ethereum.request({ method: "eth_chainId" });
 };
 
 
-let myToken = {
-  address: '0x494b2CeE761FdfCd6f5bE1ABefB7A6112B251874',
-  abi: tokenContract.abi,
-  imageBuffer: '',
-  ipfsImageHash: '',
-  ipfsImageUrl: '',
-  metadataBuffer: "",
-  ipfsMetadataHash: '',
-  ipfsMetadataUrl: '',
-  recipientAddress: '',
-  newMinterAddress: '',
-  txHash: '',
-  txReceipt: '',
-  blockNumber: '',
-  gasUsed: ''
+
+function exists(s) {
+  let chk = (s ? s.toString() : '');
+  return chk && chk.trim().length > 0;
 }
 
-let networkId = "";
-
-function handleChainChanged(_chainId) {
-  if (networkId.length) {
-    window.location.reload();
-  } else {
-    networkId = _chainId;
-  }
+function clear() {
+  window.location.reload();
 }
 
-const checkNetwork = async (provider) => {
+const checkNetwork = async (provider, vueInstance) => {
+  const self = vueInstance || this
   // first update the values that can change while connected
-  let myContext = state.web3ctx;
+  let myContext = self.web3ctx;
   let accounts = await ethereum.request({ method: "eth_requestAccounts" });
   myContext.activeWallet = accounts[0];
   myContext.lastBlockNumber = await provider.getBlockNumber();
   myContext.currentBalance = await provider.getBalance(accounts[0]);
 
-  const { name, chainId } = await provider.getNetwork();
+  const { chainId } = await provider.getNetwork();
   myContext.networkId = chainId;
-  myContext.networkName = web3networks[chainId]
-    ? web3networks[chainId].name
+  myContext.networkName = self.web3networks[chainId]
+    ? self.web3networks[chainId].name
     : "unknown";
-  myContext.etherscanUrl = web3networks[chainId]
-    ? web3networks[chainId].etherscanUrl
+  myContext.etherscanUrl = self.web3networks[chainId]
+    ? self.web3networks[chainId].etherscanUrl
     : "unknown";
 
   if (tokenContract.networks[chainId]) {
@@ -124,13 +51,13 @@ const checkNetwork = async (provider) => {
 };
 
 
-const mintToken = async () => {
+const attemptMint = async () => {
 
-  if (!App.exists(this.state.myToken.abi) ||
-      !App.exists(this.state.web3ctx.tokenAddress) ||
-      !App.exists(this.state.myToken.recipientAddress) ||
-      !App.exists(this.state.myToken.ipfsImageUrl) ||
-      !App.exists(this.state.myToken.ipfsMetadataUrl)) {
+  if (!exists(this.state.myToken.abi) ||
+      !exists(this.state.web3ctx.tokenAddress) ||
+      !exists(this.state.myToken.recipientAddress) ||
+      !exists(this.state.myToken.ipfsImageUrl) ||
+      !exists(this.state.myToken.ipfsMetadataUrl)) {
       console.log('Required input fields are missing!');
       return false;
   }
@@ -187,9 +114,88 @@ const mintToken = async () => {
 };
 
 
-var app = new Vue({
+var App = new Vue({
   el: '#wpbody',
-  data: {
-    message: 'Hello Vue!'
+  data: () => ({
+    networkId: '',
+    web3networks : {
+      1: {
+        name: "main",
+        etherscanUrl: "https://etherscan.io/tx/",
+      },
+      3: {
+        name: "ropsten",
+        etherscanUrl: "https://ropsten.etherscan.io/tx/",
+      },
+      4: {
+        name: "rinkeby",
+        etherscanUrl: "https://rinkeby.etherscan.io/tx/",
+      },
+      42: {
+        name: "kovan",
+        etherscanUrl: "https://kovan.etherscan.io/tx/",
+      },
+    },
+    web3ctx: {
+      networkId: "",
+      networkName: "",
+      etherscanUrl: "",
+      activeWallet: "",
+      lastBlockNumber: "",
+      currentBalance: "",
+      tokenAddress: "0x494b2CeE761FdfCd6f5bE1ABefB7A6112B251874",
+      ipfsGateway: "https://ipfs.infura.io/ipfs/",
+    },
+    myToken : {
+      address: '0x494b2CeE761FdfCd6f5bE1ABefB7A6112B251874',
+      abi: tokenContract.abi,
+      imageBuffer: '',
+      ipfsImageHash: '',
+      ipfsImageUrl: '',
+      metadataBuffer: "",
+      ipfsMetadataHash: '',
+      ipfsMetadataUrl: '',
+      recipientAddress: '',
+      newMinterAddress: '',
+      txHash: '',
+      txReceipt: '',
+      blockNumber: '',
+      gasUsed: ''
+    }
+  }),
+  methods: {
+    attemptMint: attemptMint,
+    exists,
+    clear,
+    handleChainChanged (_chainId) {
+      if (this.networkId.length) {
+        window.location.reload();
+      } else {
+        this.networkId = _chainId;
+      }
+    },
+    checkNetwork(provider) {
+      const self = this    
+      return checkNetwork.apply(self, [provider, self])
+    }
+  },
+  mounted () {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const signer = provider.getSigner();
+    
+    if (provider) {
+      this.checkNetwork(provider)
+        .then((e) => {
+          return startApp()
+        })
+        .then((chainId) => {
+          this.handleChainChanged(chainId);
+
+          ethereum.on("chainChanged", this.handleChainChanged);
+        })
+        .catch((e) => console.log(e));
+    }
+
   }
 })
